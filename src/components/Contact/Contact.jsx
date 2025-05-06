2081004ee2467dbfd6ea79f22234729aafe8a7b9
Ringmaster09 <priyanshchaudhary1@gmail.com>
2025-05-06 22:25:28 +0530

    1st deployement

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,31 @@ import React from 'react';
 import './Contact.css';
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "d88b4ea3-4be0-4cd9-bc92-399ec8f9340f");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     
   <div 
@@ -19,7 +44,7 @@ className="contact">
         </ul>
       </div>
       <div className="contact-col">
-        <form>
+        <form onSubmit={onSubmit}>
           <label>Your name</label>
           <input type="text" name='name' placeholder='enter your name' required/>
           <label>Phone Number</label>
@@ -28,6 +53,7 @@ className="contact">
              <textarea name="message"  rows="6" placeholder='enter your message'></textarea>   
              <button type='submit' className= 'btn dark-btn'>Submit now</button>
              </form>
+             
       </div>
     </div>
     
